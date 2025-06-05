6898f7f1f179eddd495a55691dfad7b6bcac31f6
eloso85 <eloso85@gmail.com>
2025-06-05 11:05:25 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+//Api route to serve portfolio data
+app.get("/api/portfoilo", (req, res) => {
+  const portfolioData = require("./data/portfolio.json");
+  res.json(portfolioData);
+});
+
+// Handle SPA routing - exclude files with extensions
+app.get(/^(?!.*\.[a-zA-Z0-9]+$).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Vanilla SPA app running at http://localhost:${port}`);
+  console.log("Access API data at: http://localhost:${port}/api/portfolio");
+});
+
